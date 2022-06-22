c61bd1697534385efad9082b6d95475120e5e294
vivek.padelkar <vivek.padelkar@gmail.com>
2022-06-22 15:00:46 +0530

    port added

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import express from 'express'
 import jsonServer from 'json-server'
+import dontenv from 'dotenv'
 const server = jsonServer.create()
 const router = jsonServer.router('./assets/db.json') // <== Will be created later
 const middlewares = jsonServer.defaults()
-const port = process.env.JSONPORT // <== You can change the port
+
+dontenv.config()
+const port = process.env.JSONPORT || 5024 // <== You can change the port
 
 server.use(middlewares)
 server.use(router)
 
-server.listen(port)
+server.listen(port, () => {
+  console.log('its running on port=' + port)
+})
